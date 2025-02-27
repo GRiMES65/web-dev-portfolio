5e08fe2c6d01f9f02ab7feb07f00f874ea9a5183
Jeevan <work.jeevansp@gmail.com>
2025-02-27 15:05:19 -0500

    made changes for vercel deployment

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react'
+import React from 'react'
 import SectionHeading from '@/components/section-heading/section-heading-component'
 import { projectsData } from '@/lib/data'
 import "./projects.css"
